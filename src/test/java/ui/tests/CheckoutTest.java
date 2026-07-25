fc179d234c74e2d3f8eea414ae6d8947aa2637dd
evgeniia-makarchuk <janemakar4uk@mail.ru>
2026-07-25 18:18:12 +0300

    Add checkout required field validation tests

--- src/test/java/ui/tests/CheckoutTest.java
+++ src/test/java/ui/tests/CheckoutTest.java
@@ -7,8 +7,8 @@ import ui.pages.LoginPage;
 import static data.CheckoutData.FIRST_NAME;
 import static data.CheckoutData.LAST_NAME;
 import static data.CheckoutData.POSTAL_CODE;
+import static data.CheckoutErrorMessages.*;
 import static data.users.UserType.STANDARD_USER;
-import static data.CheckoutErrorMessages.FIRST_NAME_REQUIRED;
 
 public class CheckoutTest extends BaseUiTest {
 
@@ -42,5 +42,37 @@ public class CheckoutTest extends BaseUiTest {
                 .setPostalCode(POSTAL_CODE)
                 .clickContinue()
                 .shouldShowError(FIRST_NAME_REQUIRED);
+    }
+
+    @Test
+    public void checkoutWithoutLastNameTest() {
+        new LoginPage()
+                .openPage()
+                .login(STANDARD_USER.getUsername(), STANDARD_USER.getPassword())
+                .shouldBeOpened()
+                .addBackpackToCart()
+                .openCart()
+                .shouldContainBackpack()
+                .clickCheckout()
+                .setFirstName(FIRST_NAME)
+                .setPostalCode(POSTAL_CODE)
+                .clickContinue()
+                .shouldShowError(LAST_NAME_REQUIRED);
+    }
+
+    @Test
+    public void checkoutWithoutPostalCodeTest() {
+        new LoginPage()
+                .openPage()
+                .login(STANDARD_USER.getUsername(), STANDARD_USER.getPassword())
+                .shouldBeOpened()
+                .addBackpackToCart()
+                .openCart()
+                .shouldContainBackpack()
+                .clickCheckout()
+                .setFirstName(FIRST_NAME)
+                .setLastName(LAST_NAME)
+                .clickContinue()
+                .shouldShowError(POSTAL_CODE_REQUIRED);
     }
 }
